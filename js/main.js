4fa526b90c721fd808557f210f50a3e246c1c51b
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-07-15 23:02:46 +0200

    something

--- js/main.js
+++ js/main.js
@@ -1,19 +1,36 @@
-const ratesBtns = document.querySelectorAll(".rates");
+const ratesBtns = document.querySelectorAll(".rates"); //array of all the rates
+let notClickedFlag = false;
 
-window.onload = function ratesStates(){
+window.onload = function main(){
 
     ratesBtns.forEach((rate) => {
-        rate.addEventListener("click", () => {
-            ratesBtns.forEach((oldRate) => {
-                if(oldRate.getAttribute("data-clicked") == "true"){
-                    oldRate.setAttribute("data-clicked", "false");
-                    rate.setAttribute("data-clicked", "true");
-                }
-            })
-
-            rate.setAttribute("data-clicked", "true");
-
-        })
+        rate.addEventListener("click", () => handlingClicking(rate));
+        rate.addEventListener("mouseover", () => handlingMouseOver(rate));
+        rate.addEventListener("mouseout", () => handlingMouseOut(rate));
     })
 
+}
+
+function handlingClicking(button){
+    button.classList.add("current_rate");
+    button.setAttribute("data-clicked", "true");
+}
+
+function handlingMouseOver(button){
+    ratesBtns.forEach((ryba) => {
+        if(ryba.getAttribute("data-clicked") == "true" && button.getAttribute("data-clicked") != "true"){
+            notClickedFlag = true;
+
+            ryba.classList.add("old_rate");
+            ryba.classList.remove("current_rate");
+            button.classList.add("current_rate");
+        }
+    })
+}
+
+function handlingMouseOut(button){
+    if(notClickedFlag){
+        button.classList.remove("current_rate");
+
+    }
 }
