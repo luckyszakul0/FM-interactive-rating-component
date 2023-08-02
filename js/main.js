f82a4b79ecba2e4e6eb1e34a26c26c6c56fde422
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-08-02 22:48:03 +0200

    added some comments

--- js/main.js
+++ js/main.js
@@ -12,10 +12,11 @@ window.onload = function main(){
 }
 
 function handlingClicking(button){
-    //
+    //changed the clicked button's colors
     button.classList.add("current_rate");
     button.setAttribute("data-clicked", "true");
 
+    //remove all of the dynamic classes from the old clicked button if it exists
     if(clickedIndex != null){
         ratesBtns[clickedIndex].classList.remove("current_rate");
         ratesBtns[clickedIndex].classList.remove("old_rate");
@@ -26,6 +27,7 @@ function handlingClicking(button){
 }
 
 function handlingMouseOver(button){
+    //if mouse over new button - give it orange color and give the light gray color to the old clicked button
     button.classList.add("current_rate");
 
     if(clickedIndex != null)
@@ -33,9 +35,11 @@ function handlingMouseOver(button){
 }
 
 function handlingMouseOut(button){
+    //if the button hasn't been clicked - remove it's orange color on mouse out
     if(button.getAttribute("data-clicked") == "false")
         button.classList.remove("current_rate");
 
+    //give the old clicked button it's orange color back
     if(clickedIndex != null)
         ratesBtns[clickedIndex].classList.remove("old_rate");
 }
