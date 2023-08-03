8906f6f74b9c5f195ffaf24e015f02bc4fa0b5c7
Lukasz710 <lubaszewskilukasz0@gmail.com>
2023-08-03 19:58:34 +0200

    finished the dynamic styles logic

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 const ratesBtns = document.querySelectorAll(".rates"); //array of all the rates
-let clickedIndex = null;
+let clickedIndex = 'null'; //index of currently clicked button, on the start it's a string, then changes to integer
 
 window.onload = function main(){
 
+    //main function - add event listeners to all of the rates button
     ratesBtns.forEach((rate) => {
         rate.addEventListener("mouseover", () => handlingMouseOver(rate));
         rate.addEventListener("mouseout", () => handlingMouseOut(rate));
@@ -16,8 +17,8 @@ function handlingClicking(button){
     button.classList.add("current_rate");
     button.setAttribute("data-clicked", "true");
 
-    //remove all of the dynamic classes from the old clicked button if it exists
-    if(clickedIndex != null){
+    //remove all of the dynamic classes from the old clicked button if it exists and it isn't the one which is being clicked
+    if(clickedIndex !== 'null' && button.getAttribute("data-index") !== clickedIndex){
         ratesBtns[clickedIndex].classList.remove("current_rate");
         ratesBtns[clickedIndex].classList.remove("old_rate");
         ratesBtns[clickedIndex].setAttribute("data-clicked", "false");
@@ -29,8 +30,9 @@ function handlingClicking(button){
 function handlingMouseOver(button){
     //if mouse over new button - give it orange color and give the light gray color to the old clicked button
     button.classList.add("current_rate");
-
-    if(clickedIndex != null)
+    
+    //change the color of the old button to light gray if the old one exists and it isn't the one being hovered at the moment
+    if(clickedIndex !== 'null' && button.getAttribute("data-index") !== clickedIndex)
         ratesBtns[clickedIndex].classList.add("old_rate");
 }
 
@@ -40,6 +42,6 @@ function handlingMouseOut(button){
         button.classList.remove("current_rate");
 
     //give the old clicked button it's orange color back
-    if(clickedIndex != null)
+    if(clickedIndex !== 'null')
         ratesBtns[clickedIndex].classList.remove("old_rate");
 }
